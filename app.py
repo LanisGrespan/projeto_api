de51df58d9086a09bb030f64c8232c006b278178
grespan <alanis.grespan.sesi@gmail.com>
2024-04-15 15:44:14 -0300

    Atualização 2.1

--- app.py
+++ app.py
@@ -23,18 +23,39 @@ def argumentos(nome:str):
 def arg_implicito(nome:str):
     return jsonify( {"status": 200, "data":nome} )
 
-@app.route("/idades", methods = ("GET"))
+@app.route("/idades", methods = ("GET",))
 def idades():
     from random_data import pessoas
     import funcoes
     num = funcoes.maior_de_50(pessoas)
     return jsonify( {"status": 200, "data": num} )
 
-@app.route("/salario", methods = ("GET"))
+@app.route("/salario", methods = ("GET",))
 def salario():
     from random_data import pessoas
     import funcoes
     num = funcoes.mais_2000(pessoas)
+    return jsonify( {"status": 200, "data": num} )
+
+@app.route("/maiores_salarios", methods = ("GET",))
+def dados():
+    from random_data import pessoas
+    import funcoes
+    num = funcoes.maior_salario(pessoas)
+    return jsonify( {"status": 200, "data": num} )
+
+@app.route("/salario_media", methods = ("GET",))
+def media_salarial():
+    from random_data import pessoas
+    import funcoes
+    num = funcoes.media_profissoes(pessoas)
+    return jsonify( {"status": 200, "data": num} )
+
+@app.route("/salario_idade", methods = ("GET",))
+def intervalo():
+    from random_data import pessoas
+    import funcoes
+    num = funcoes.maior_2000_sexo(pessoas)
     return jsonify( {"status": 200, "data": num} )
 
 if __name__ == '_main_':
