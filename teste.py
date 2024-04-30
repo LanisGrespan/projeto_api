fa9b637d61592560303ba05e4fb59f41655c63a2
grespan <alanis.grespan.sesi@gmail.com>
2024-04-30 14:11:33 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,14 +1,8 @@
-#programa distancia
+#Apenas um teste 
 
-<<<<<<< HEAD
-=======
-#teste 
-
->>>>>>> tutorial_configuracao
-motorista = input("Digite o nome do motorista: ")
 origem = input("Cidade de origem: ")
 destino = input("Cidade de destino: ")
 distancia = float(input("Qual a distância em km? "))
 velocidade = float(input("Quantos hm/h irá viajar? "))
 tempo = distancia/velocidade*60
-print(f"{motorista} levará {tempo}h de {origem} à {destino}")
+print(f" levará {tempo}h de {origem} à {destino}")
