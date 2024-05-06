cc5e3217ebe2b1b72453bd5a2765df75490cbd91
LanisSenai <alanis.grespan.sesi@gmail.com>
2024-05-06 08:06:20 -0300

    teste.py deletado

--- teste.py
+++
@@ -1,8 +0,0 @@
-#Apenas um teste 
-
-origem = input("Cidade de origem: ")
-destino = input("Cidade de destino: ")
-distancia = float(input("Qual a distância em km? "))
-velocidade = float(input("Quantos hm/h irá viajar? "))
-tempo = distancia/velocidade*60
-print(f" levará {tempo}h de {origem} à {destino}")
