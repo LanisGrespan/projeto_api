e203e3efb28dc7378130e0cde7502ffb4afae8ce
LanisSenai <alanis.grespan.sesi@gmail.com>
2024-05-07 12:08:57 -0300

    Rota index e main

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, jsonify
-from random_data import pessoas
+from flask import Flask, render_template
 
 # FLASK é um mine-framework (gerencia as rotas)
 
 app = Flask(__name__)
 
+app.route("/")
+def index():
+    return render_template("index.html")
+
 from api import bp
 app.register_blueprint(bp)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
